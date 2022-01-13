371c510ce9e3deb2a016e2554278ac0d523c7706
Tashisam07 <tashisam2057@gmail.com>
2022-01-13 19:54:35 +0530

    cryptoverse

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': '389b73545cmsh50e959c5c7850fdp147cf4jsn54d7d8580df7',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins'),
+    })
+  })
+});
+export const { useGetCryptosQuery, } = cryptoApi;
